df6c99b4afe7e4dd443bdaa4bddecb46dd434883
Sylvain Henry <sylvain@haskus.fr>
2021-03-22 16:52:12 +0100

    Commit missing file

---
+++ src/Haskus/Had/GitLab.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments #-}
+
+module Haskus.Had.GitLab where
+
+import qualified Data.Text as Text
+
+import GitLab
+import GitLab.WebRequests.GitLabWebCalls
+
+import Haskus.Had.State
+
+gitlabRequest :: State -> GitLab m -> IO m
+gitlabRequest s act = runGitLab (stateGitlab s) act
+
+getGhcProject :: State -> IO Project
+getGhcProject s = do
+  r <- gitlabRequest s (GitLab.searchProjectId 1)
+  case r of
+    Left err       -> error $ "Gitlab connection issue: " <> show err
+    Right Nothing  -> error $ "Couldn't find GHC project (id 1)"
+    Right (Just v) -> return v
+
+-- | Get active Milestones
+getMilestones :: State -> Project -> IO [Milestone]
+getMilestones s p = gitlabRequest s (gitlabWithAttrsUnsafe path opts)
+  where
+    path = "/projects/" <> Text.pack (show (GitLab.project_id p)) <> "/milestones"
+    opts = "&state=active"
+
+-- | Get number of issues associated with a milestone
+getMilestoneIssueCount :: State -> Project -> Milestone -> IO (Maybe Stats)
+getMilestoneIssueCount s p m = gitlabRequest s (gitlabWithAttrsOneUnsafe path opts)
+  where
+    path = "/projects/" <> Text.pack (show (GitLab.project_id p)) <> "/issues_statistics"
+    opts = "&milestone=" <> milestone_title m
+
+-- | Get number of open issues associated with a milestone
+getMilestoneOpenIssueCount :: State -> Project -> Milestone -> IO Word
+getMilestoneOpenIssueCount s p m = do
+  Just r <- getMilestoneIssueCount s p m
+  return (statistics_counts_opened . statistics_counts . statistics $ r)
